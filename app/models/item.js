3b5e6d8c430361a6076ac7ad1b75ebd50eaceb50
DSRoden <Daniel.S.Roden@gmail.com>
2014-07-23 14:54:29 -0500

    added total value function

--- app/models/item.js
+++ app/models/item.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var _ = require('lodash');
 var cItem = global.mongodb.collection('items');
 
 function Item(name, room, acquired, count, cost){
@@ -19,6 +19,22 @@ Item.prototype.save = function (cb) {
 Item.find = function(query, cb){
   cItem.find(query).toArray(function(err, items){
   cb(items);  
+  });
+};
+
+Item.prototype.value = function (count, cost) {
+  return this.count * this.cost;
+};
+
+Item.totalValue = function(query, cb){
+  Item.find(query, function(items){
+    var sum = 0;
+    for (var i = 0; i < items.length; i++){
+      var item = items [i];
+      item = _.create(Item.prototype, item);
+      sum += item.value();
+    }
+    cb(sum);
   });
 };
 
